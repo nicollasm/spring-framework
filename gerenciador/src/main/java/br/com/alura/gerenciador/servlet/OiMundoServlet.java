03fc4442090ea120f0b21e8d26448c2bf7beb8ee
Nicollas Martins <82549631+nicollasm@users.noreply.github.com>
2022-09-07 13:19:42 -0300

    .xml updated

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -9,8 +9,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 //oi
-@WebServlet(urlPatterns = "/oi")
+//@WebServlet(urlPatterns = "/oi")
 public class OiMundoServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
