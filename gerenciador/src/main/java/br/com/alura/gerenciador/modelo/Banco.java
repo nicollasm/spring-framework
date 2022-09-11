ada9e0f8675cc3b8b49d716ec8d9ac3b298ae287
Nicollas Martins <82549631+nicollasm@users.noreply.github.com>
2022-09-11 15:42:07 -0300

    feature: validating httpsession

--- gerenciador/src/main/java/br/com/alura/gerenciador/modelo/Banco.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 public class Banco {
 
 	private static final List<Empresa> lista = new ArrayList<>();
+	private static final List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 
 	static {
@@ -18,6 +19,16 @@ public class Banco {
 		empresa2.setNome("Caelum");
 		lista.add(empresa);
 		lista.add(empresa2);
+
+		Usuario u1 = new Usuario();
+		u1.setLogin("Nicollas");
+		u1.setSenha("12345");
+		Usuario u2 = new Usuario();
+		u2.setLogin("Nico");
+		u2.setSenha("12345");
+
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 	}
 
 	public void adiciona(Empresa empresa) {
@@ -40,6 +51,16 @@ public class Banco {
 			}
 
 		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for (Usuario usuario : listaUsuarios) {
+			if (usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}
+
 		return null;
 	}
 
