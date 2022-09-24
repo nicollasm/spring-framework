28b3bfb7826866457c0dd75677b27d9ec334dc3d
Nicollas Martins <82549631+nicollasm@users.noreply.github.com>
2022-09-24 14:56:10 -0300

    feature: list produtos

--- loja-virtual-repository/src/TestaListagem.java
+++ loja-virtual-repository/src/TestaListagem.java
@@ -1,17 +1,34 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TestaListagem {
 
 	public static void main(String[] args) throws SQLException {
-		
+
 		Connection con = DriverManager.getConnection(
 				"jdbc:mysql://localhost/loja_virtual?useTimezone=true&serverTimezone=UTC", "root", "root");
 
 		System.out.println("Fechando conexão");
 
+		Statement stm = con.createStatement();
+		stm.execute("SELECT ID, NOME, DESCRICAO FROM PRODUTO");
+
+		ResultSet rst = stm.getResultSet();
+		
+		while(rst.next()) {
+			Integer id = rst.getInt("ID");
+			System.out.println(id);
+			String nome = rst.getString("NOME");
+			System.out.println(nome);
+			String descricao = rst.getString("DESCRICAO");
+			System.out.println(descricao);
+		}
+		
 		con.close();
+
 	}
-	
+
 }
