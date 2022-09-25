36467705e088cefbf30bf4ceb3b482a39ff3164f
Nicollas Martins <82549631+nicollasm@users.noreply.github.com>
2022-09-25 16:03:25 -0300

    feature: using preparedstatement to threat the parameters

--- loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -1,4 +1,5 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -7,18 +8,16 @@ public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
 
-		String nome = "MOUSE";
+		String nome = "MOUSE'";
 		String descricao = "MOUSE SEM FIO";
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
-		
-		String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES ('" + nome + "', '" + descricao + "')";
-		
-		System.out.println(sql);
 
-		Statement stm = connection.createStatement();
-		stm.execute("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES ('" + nome + "', '" + descricao + "')",
-				Statement.RETURN_GENERATED_KEYS);
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (? , ?)", Statement.RETURN_GENERATED_KEYS);
+		
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		stm.execute();
 
 		ResultSet rst = stm.getGeneratedKeys();
 		while (rst.next()) {
