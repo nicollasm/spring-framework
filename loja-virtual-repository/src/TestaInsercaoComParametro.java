f5d09253c7249f8348265ce7656e80bc36421948
Nicollas Martins <82549631+nicollasm@users.noreply.github.com>
2022-09-25 18:53:36 -0300

    feature: setting max pool size

--- loja-virtual-repository/src/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -7,24 +7,42 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-
-		String nome = "MOUSE'";
-		String descricao = "MOUSE SEM FIO";
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection connection = factory.recuperarConexao();
+		try (Connection connection = factory.recuperarConexao()) {
+			connection.setAutoCommit(false);
+			try (PreparedStatement stm = connection.prepareStatement(
+					"INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (? , ?)", Statement.RETURN_GENERATED_KEYS)) {
+				adicionarVariavel("SMART TV", "45 POLEGADAS", stm);
+				adicionarVariavel("RADIO", "RADIO A PILHA", stm);
 
-		PreparedStatement stm = connection.prepareStatement("INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (? , ?)", Statement.RETURN_GENERATED_KEYS);
-		
+				connection.commit();
+
+				stm.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+
+				connection.rollback();
+			}
+		}
+	}
+
+	private static void adicionarVariavel(String nome, String descricao, PreparedStatement stm) throws SQLException {
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
+//
+//		if (nome.equals("RADIO")) {
+//			throw new RuntimeException("Não foi possivel adicionar o produto");
+//		}
+
 		stm.execute();
 
-		ResultSet rst = stm.getGeneratedKeys();
-		while (rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O ID criado foi: " + id);
+		try (ResultSet rst = stm.getGeneratedKeys()) {
+			while (rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O ID criado foi: " + id);
+			}
 		}
-
 	}
 
 }
