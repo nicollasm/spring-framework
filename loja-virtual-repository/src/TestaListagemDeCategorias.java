7a4358aefcdcb5c2aaa0cb2ac8c59e5c9becd895
Nicollas Martins <82549631+nicollasm@users.noreply.github.com>
2022-10-01 16:31:46 -0300

    feature: updating

---
+++ loja-virtual-repository/src/TestaListagemDeCategorias.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+import br.com.alura.jdbc.ConnectionFactory;
+import br.com.alura.jdbc.dao.CategoriaDAO;
+import br.com.alura.jdbc.modelo.Categoria;
+
+public class TestaListagemDeCategorias {
+
+	public static void main(String[] args) throws SQLException {
+
+		try (Connection connection = new ConnectionFactory().recuperarConexao()) {
+			CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
+			List<Categoria> listaDeCategoria = categoriaDAO.listar();
+			listaDeCategoria.stream().forEach(ct -> System.out.println(ct.getNome()));
+
+		}
+
+	}
+
+}
---
+++ loja-virtual-repository/src/br/com/alura/jdbc/modelo/Categoria.java
@@ -0,0 +1,18 @@
+package br.com.alura.jdbc.modelo;
+
+public class Categoria {
+
+	private Integer id;
+	private String nome;
+	
+	public Categoria(Integer id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	
+	}
+
+	public Object getNome() {
+		return nome;
+	}
+	
+}
